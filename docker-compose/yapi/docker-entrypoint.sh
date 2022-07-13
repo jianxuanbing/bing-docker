2116690babcba36ff84e6383230be43bd89699c1
jian玄冰 <jianxuanhuo1@126.com>
2022-07-13 17:37:13 +0800

    feat: 新增 yapi 容器编排

---
+++ docker-compose/yapi/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# yapi初始化后会有一个init.lock文件
+lockPath="/api/init.lock"
+
+# 进入yapi项目
+cd /api/vendors
+ls -a
+
+# 如果初始化文件文件存在,则直接运行,否则初始化
+if [ ! -f "$lockPath" ]
+then
+  # 启动Yapi初始化
+  node server/install.js
+  # 若是初始化成功的情况下直接运行yapi
+  node server/app.js
+else
+  # 运行yapi管理系统
+  node server/app.js
+fi
